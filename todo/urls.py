7fde2cdc8f7d0bb21f40356fa0837da984a02d2a
vostbur <vostbur@gmail.com>
2021-05-24 16:03:24 +0300

    add create todo functionality

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('add/', views.add_todo, name='add'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .models import Todo
+
+from .forms import TodoForm
 
 
 def home_page(request):
-    return render(request, 'todo/home_page.html')
+    todos = Todo.objects.filter(user=request.user, completed__isnull=True)
+    return render(request, 'todo/home_page.html', {'todos': todos})
+
+
+def add_todo(request):
+    if request.method == 'GET':
+        return render(request, 'todo/add.html', {'form': TodoForm()})
+    else:
+        try:
+            form = TodoForm(request.POST)
+            todo = form.save(commit=False)
+            todo.user = request.user
+            todo.save()
+            return redirect('home')
+        except ValueError:
+            return render(request, 'todo/add.html',
+                          {'form': TodoForm(),
+                           'error': 'There is an error in the data. Please try again.'})
